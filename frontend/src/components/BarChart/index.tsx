e4023994cc57450b6514e2922cf3926a38dab656
Thiago Fonseca <thifonseca85@gmail.com>
2021-09-11 20:09:52 -0300

    Env Var

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -39,7 +39,7 @@ const BarChat = () => {
 
                 setChartData({
                     labels: {
-                    categories: []
+                    categories: myLabels
                 },
                 series: [
                     {
